06dff10002810d25a30bcf05bf1f40c1f1b928b2
JaxDoge <niftyjax@gmail.com>
2022-11-15 17:00:26 -0800

    MarketChartController

--- Controller/SearchPageControllers/SearchController.js
+++ Controller/SearchPageControllers/SearchController.js
@@ -5,9 +5,9 @@ const coinGeckoSearch_API = "https://api.coingecko.com/api/v3/search?query="
 
 const SearchController = (app) => {
 
-    function searchCoinGecko(req, res) {
+    async function searchCoinGecko(req, res) {
         const query = req.query.query;
-        axios.get(`${coinGeckoSearch_API}${query}`)
+        await axios.get(`${coinGeckoSearch_API}${query}`)
             .then((response) => {
                 res.json(response.data.coins);
             })
