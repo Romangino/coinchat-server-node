2669a75ccfd1e962cccddf308a0851f78a00792f
Gino <romangino@gmail.com>
2022-11-28 23:23:41 -0500

    - Added Mongo database to server.js - Created Watchlist collection with model, schema, dao, and controller

---
+++ watchlists/watchlist-controller.js
@@ -0,0 +1,26 @@
+import * as watchlistDao from './watchlist-dao.js'
+
+const findUserWatchlist = async (req, res) => {
+    const uid = req.params.uid
+    const watchlist = await watchlistDao.findUserWatchlist(uid)
+    res.json(watchlist)
+}
+
+const addToWatchlist = async (req, res) => {
+    const newItem = req.body
+    const addedItem = await watchlistDao.addToWatchlist(newItem)
+    res.json(addedItem)
+}
+
+const removeFromWatchlist = async (req, res) => {
+    const wid = req.params.wid
+    const status = await watchlistDao
+        .removeFromWatchlist(wid)
+    res.json(status)
+}
+
+export default (app) => {
+    app.get('/api/users/:uid/watchlist', findUserWatchlist)
+    app.post('/api/watchlist', addToWatchlist)
+    app.delete('/api/watchlist/:wid', removeFromWatchlist)
+}
