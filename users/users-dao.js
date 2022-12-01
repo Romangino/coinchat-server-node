2381d465b102425dfa301aa971e3ba730601eb1f
Riccardo Prosdocimi <riccardo.prosdocimi@gmail.com>
2022-12-01 00:14:17 -0500

    users done

--- users/users-dao.js
+++ users/users-dao.js
@@ -3,9 +3,9 @@ import usersModel from "./users-model.js";
 export const createUser = async user => await usersModel.create(user);
 export const findAllUsers = async () => await usersModel.find();
 export const findUserById = async uid => await usersModel.findById(uid);
-export const findByUsername = async email => await usersModel.findOne({email});
-export const findByCredentials = async (handle, password) => await usersModel
-    .findOne({handle, password}, {password: false});
+export const findUserByUsername = async email => await usersModel.findOne({email});
+export const findByCredentials = async ({email, password}) => await usersModel
+    .findOne({email, password}, {password: false});  // TODO: ask about projection
 export const deleteUser = async uid => await usersModel.deleteOne({_id: uid});
 export const updateUser = async (uid, userUpdates) => await usersModel
     .updateOne({_id: uid}, {$set: userUpdates});
