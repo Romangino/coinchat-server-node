ad304a5adddc06db4df2927933bea47f365bbb64
JaxDoge <niftyjax@gmail.com>
2022-12-05 22:40:50 -0800

    implement comment service, now it can post and fetch comment from remote server

--- schemas/users-follow-users-schema.js
+++ schemas/users-follow-users-schema.js
@@ -5,7 +5,8 @@ const usersFollowUsersSchema = Schema({
     userID: {type: Schema.Types.ObjectId, ref: 'UsersModel', required: true},  // followee ID
     followedUserID: {type: Schema.Types.ObjectId, ref: 'UsersModel', required: true}, //  follower ID
     },
-    { timestamps: true },{collection: 'users-follow-users'}
+    {collection: 'users-follow-users'},
+    { timestamps: true }
 )
 
 export default usersFollowUsersSchema
