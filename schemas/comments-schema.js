b8fed19e1fe14e022be75a3093dcac1a16457e3c
jaxtan <niftyjax@gmail.com>
2022-12-09 22:36:54 -0500

    -Implement add to watchlist button in detail page -Implement redirect to user profile from detail page

--- schemas/comments-schema.js
+++ schemas/comments-schema.js
@@ -5,7 +5,7 @@ const commentSchema = Schema({
     // default id is the comment ID
     authorID: {type: Schema.Types.ObjectId, ref: 'UsersModel', required: true},  // Same as objectID in user collection
     objectID: {type: String, required: true},    //  Comment object ID base on object type could be coin ID or post ID
-    objectType: {type: String, required: true, enum: ["Coin", "Post"]},   // Comment object type
+    objectType: {type: String, required: true, enum: ["Coin", "Blog"]},   // Comment object type
     detailContent: {type: String, required: true},    //  Content of this comment
     likes: {type: Number, required: true, default: 0},   //  Like numbers
     dislikes: {type: Number, required: true, default: 0},   // Dislike numbers
