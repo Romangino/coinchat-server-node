45480da7ee6997cb04ad05fa988370659fafe989
Riccardo Prosdocimi <riccardo.prosdocimi@gmail.com>
2022-12-16 12:48:05 -0500

    production stage

--- database-model/users-model.js
+++ database-model/users-model.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 import usersSchema from "../schemas/users-schema.js";
+import blogsModel from "./blogs-model.js";
+import commentModel from "./comments-model.js";
+import followModel from "./follow-model.js";
+import usersReactCommentModel from "./users-react-comment-model.js";
+import watchlistModel from "./watchlist-model.js";
 
+// delete all data related to a user
+usersSchema.post("deleteOne", async function() {
+    const userID = this.getQuery()._id;
+    await blogsModel.deleteMany({authorID: userID});
+    await commentModel.deleteMany({authorID: userID});
+    await followModel.deleteMany({$or: [{followee: userID}, {follower: userID}]});
+    await usersReactCommentModel.deleteMany({userID: userID});
+    await watchlistModel.deleteMany({uid: userID});
+});
 const usersModel = mongoose.model(
     'UsersModel', usersSchema
 );
